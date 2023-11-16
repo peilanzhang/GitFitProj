177c73db991eb5d6b9979251e33828d2a6819c82
Peilan Zhang <lan.zhang9534@gmail.com>
2023-11-16 16:02:24 -0600

    create workoutPage

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,38 +1,57 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import workoutPage from './workoutPage'; 
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function HomePage() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Today's Workout: {'\n'}</Text>
-      
       <Text style={styles.target}>Push{'\n'}</Text>
       <View style={styles.workoutContent}>
-      <Text>Workout 1{'\n'}</Text>
-      <Text>Workout 2{'\n'}</Text>
-      <Text>Workout 3{'\n'}</Text>
-      <Text>Workout 4{'\n'}</Text>
+        <Text>Workout 1{'\n'}</Text>
+        <Text>Workout 2{'\n'}</Text>
+        <Text>Workout 3{'\n'}</Text>
+        <Text>Workout 4{'\n'}</Text>
       </View>
+
+      {/* <TouchableOpacity onPress={handleButtonPress} style={styles.button}>
+        <Text style={styles.buttonText}>Start Workout!</Text>
+      </TouchableOpacity> */}
     </View>
-    
   );
 }
 
-  
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      paddingTop: 20, // Add padding to the top to push the content down
-      paddingHorizontal: 16, // Add horizontal padding for better readability
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-    },
-    target: {
-        fontSize: 16, 
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  target: {
+    fontSize: 16,
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#566263',
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  workoutContent: {
+    marginTop: 20,
+  },
+});
---
+++ workoutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function workoutPage() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+    
+  );
+}
