1d201a0dddab685fe0f70357a4e40740e81d156c
William Salton <william.salton2020@gmail.com>
2023-12-10 23:02:53 -0600

    Upper and Lower toolbar dark mode  - Some gap in the middle of the toolbar that I cant seem to fill

--- toolbars/lowerToolbar.jsx
+++ toolbars/lowerToolbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'; // or 'react' for web apps
 import { useNavigation } from '@react-navigation/native';
+import { useTheme } from '../Theme/themeContext';
 import {Picker} from '@react-native-picker/picker'
 import InputPage from '../screens/inputExcersise';
 
@@ -17,25 +18,45 @@ function LowerToolbar() {
     const handleList = () => {
         navigation.navigate('ExerciseList');
       }
+    const { isDarkMode } = useTheme();
+
+    const styles = StyleSheet.create({
+        toolbar: {
+          flexDirection: 'row',
+          justifyContent: 'space-between', // Align icons to start and end of the container
+          alignItems: 'center',
+          padding: 40,
+          height: 100,
+          borderTopWidth: 1, // Set the width of the top border
+          borderTopColor: 'black',
+          backgroundColor: isDarkMode ? '#696a80' : '#fff',
+        },
+       
+        icon: {
+          width: 50,
+          height: 50, 
+        },
+    });
+
   return (
-    <View style={toolbarStyle.toolbar}>
+    <View style={styles.toolbar}>
       <TouchableOpacity onPress={handleGenerator}>
-      <Image source={require('./dumbell.png')} style={toolbarStyle.icon} />
+      <Image source={require('./dumbell.png')} style={styles.icon} />
       </TouchableOpacity>
 
 
       <TouchableOpacity onPress={handleInputWorkout}>
-        <Image source={require('./pencil.png')} style={toolbarStyle.icon} />
+        <Image source={require('./pencil.png')} style={styles.icon} />
         </TouchableOpacity>
 
 
         <TouchableOpacity onPress={handleList} >
-        <Image source={require('./list.png')} style={toolbarStyle.icon} />
+        <Image source={require('./list.png')} style={styles.icon} />
         </TouchableOpacity>
     </View>
   );
 }
-const toolbarStyle = StyleSheet.create({
+const styles = StyleSheet.create({
     toolbar: {
       flexDirection: 'row',
       justifyContent: 'space-between', // Align icons to start and end of the container
--- toolbars/upperToolbar.jsx
+++ toolbars/upperToolbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'; // or 'react' for web apps
 import { useNavigation } from '@react-navigation/native';
+import { useTheme } from '../Theme/themeContext';
 
 
 const UpperToolbar = () => {
@@ -13,43 +14,44 @@ const UpperToolbar = () => {
   const handleSettings = () => {
     navigation.navigate('Settings')
   };
-  
+
+  const { isDarkMode } = useTheme();
+
+  const styles = StyleSheet.create({
+    toolbar: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 10,
+      paddingTop: 40, // Add paddingTop to push content below the toolbar
+      borderBottomColor: 'black',
+      backgroundColor: isDarkMode ? '#696a80' : '#fff',
+    },
+    title: {
+      // Add styles for the title text
+    },
+    icon: {
+      width: 30,
+      height: 30,
+    },
+    spacer: {
+      flex: 1,
+    },
+  });
+
   return (
     <View style={styles.toolbar}>
       <TouchableOpacity onPress={handleNavigateHome}>
-      <Image source={require('./home-icon.png')} style={styles.icon} />
+        <Image source={require('./home-icon.png')} style={styles.icon} />
       </TouchableOpacity>
 
-      <View style={styles.spacer}/>
+      <View style={styles.spacer} />
 
       <TouchableOpacity onPress={handleSettings}>
         <Image source={require('./settings-icon.jpeg')} style={styles.icon} />
-        </TouchableOpacity>
+      </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  toolbar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between', // Align icons to start and end of the container
-    alignItems: 'center',
-    padding: 10,
-    zIndex: 1,
-    marginTop: 40,
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  title: {
-    // Add styles for the title text
-  },
-  icon: {
-    width: 30,
-    height: 30, 
-  },
-  spacer: {
-    flex: 1,
-  }
-});
 
 export default UpperToolbar;
