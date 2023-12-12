5128c83bab7fd0696bd839354c98a9eece70b35d
Jerry Xiang <jerrx221@gmail.com>
2023-12-12 01:47:24 -0600

    add delete function for exercise list

--- screens/excersiseList.jsx
+++ screens/excersiseList.jsx
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, StyleSheet, View, TouchableOpacity, Linking } from 'react-native';
+import { ScrollView, Text, StyleSheet, View, TouchableOpacity, Linking,TextInput,Button , Alert} from 'react-native';
 import { useTheme } from '../Theme/themeContext';
 
 const ExerciseList = () => {
   const { isDarkMode } = useTheme();
+  const [deleteName, setDeleteName] = useState('');
+
+  const handleDelete = () => {
+    fetch('http://10.141.144.205:3000/api/deleteExercise', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: deleteName }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // You might want to refresh the exercises list here
+      setExercises(exercises.filter(exercise => exercise.name !== deleteName));
+
+    })
+    .catch(error => console.error('Error:', error));
+  };
 
   const styles = StyleSheet.create({
     container: {
@@ -48,6 +67,27 @@ const ExerciseList = () => {
       fontSize: 18,
       textDecorationLine: 'underline',
     },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      color: isDarkMode ? '#fff' : '#000',
+    },
+    button: {
+      // Define your button styles here
+      backgroundColor: '#4D9DFF', // example background color
+      padding: 10,
+      borderRadius: 5,
+    },
+    buttonText: {
+      color: 'red', // Red text color
+      textAlign: 'center',
+    },
+    inputText: {
+      color: isDarkMode ? '#fff' : '#000',
+
+    }
   });
 
   // Component for each exercise
@@ -93,7 +133,14 @@ const handlePress = (exercise) => {
   // Example URL - replace with your actual URL or a property from 'exercise'
   const url = exercise;
 
-  Linking.openURL(url).catch(err => console.error('An error occurred', err));
+  // Linking.openURL(url).catch(err => console.error('An error occurred', err));
+  Linking.openURL(url).catch(err => {
+    Alert.alert(
+      "Error",
+      "An error occurred: " + err.message,
+      [{ text: "OK" }]
+    );
+  });
 };
 
 
@@ -102,6 +149,15 @@ const handlePress = (exercise) => {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.categoryTitle}>Exercises</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setDeleteName}
+        value={deleteName}
+        placeholder="Enter name to delete"
+      />
+      <TouchableOpacity onPress={handleDelete}>
+      <Text style={styles.buttonText}>Delete Exercise</Text>
+    </TouchableOpacity>
       {exercises.sort((a, b) => a.muscle.localeCompare(b.muscle)).map(exercise => (
         <TouchableOpacity key={exercise.name} onPress={() => handlePress(exercise.link)} style={styles.exerciseTouchable}>
 <Text style={styles.exerciseItem}>
